5d1af3221835e36e24cb690fafdfc7d3424df92d
Dayana <dayanajabif@gmail.com>
2018-09-03 13:58:21 -0300

    translate comment

--- server.ts
+++ server.ts
@@ -44,7 +44,8 @@ app.get('*', (req, res) => {
   const supportedLocales = ['en', 'es'];
   const defaultLocale = 'en';
   const matches = req.url.match(/^\/([a-z]{2}(?:-[A-Z]{2})?)\//);
-  //Si la url que viene esta ok devuelve es o en
+
+  //check if the requested url has a correct format '/locale' and matches any of the supportedLocales
   const locale = (matches && supportedLocales.indexOf(matches[1]) !== -1) ? matches[1] : defaultLocale;
 
   res.render(`${locale}/index`, { req });
